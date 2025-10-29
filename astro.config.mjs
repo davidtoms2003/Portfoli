ba2054f6adf6ebf5dfb133ee083b5c41fd5ef150
David Tomás Velasco <david@example.com>
2025-10-30 00:03:52 +0100

    Configure Pages base and fix asset paths; remove portfoli

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+	// Set your production site and base path for GitHub Pages project sites
+	site: 'https://u1979286.github.io/portfoli/',
+	base: '/portfoli/',
 	devToolbar: {
 		enabled: false,
 	},
